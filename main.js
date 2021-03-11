2380368e52325546aff3594f894e3fdaa11e7e23
spenxeram <spenceraustinmartin@gmail.com>
2021-03-11 08:34:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-// API
+// This app gets BTC exchange rates and outputs them
+// every 30 seconds from:
 //https://api.coindesk.com/v1/bpi/currentprice/vnd.json;
+
+// getting and setting global vars
+let currencies = {
+  usd: {
+    prev: 0,
+    current: 0
+  },
+  jpy: {
+    prev: 0,
+    current: 0
+  },
+  vnd: {
+    prev: 0,
+    current: 0
+  }
+}
+
+// Event Listeners
+
+
+
+
+// AJAX call to exchange
+
+
+
+// General functions for output
+
+
+// Helper functions
