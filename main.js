0b55903065038f563c8896e4ed5f93c93664e70b
spenxeram <spenceraustinmartin@gmail.com>
2021-03-11 08:48:59 +0700

    Ajax call

--- main.js
+++ main.js
@@ -18,13 +18,31 @@ let currencies = {
   }
 }
 
+// on page load
+function updateCurrencies() {
+Object.keys(currencies).forEach((currency) => {
+  getBTCVal(currency);
+});
+}
+
+updateCurrencies();
 // Event Listeners
 
-
-
-
 // AJAX call to exchange
-
+function getBTCVal(currency) {
+  let url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`;
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    url,
+    true
+  );
+  xhr.onload = function() {
+    let result = JSON.parse(this.responseText);
+    console.log(result);
+  }
+  xhr.send();
+}
 
 
 // General functions for output
